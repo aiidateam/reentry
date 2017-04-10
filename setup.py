27981bb6d0e4688a82b457a974199a45bdca2359
DropD <r.haeuselmann@gmx.ch>
2017-04-10 18:19:15 +0200

    remove name=main check from setup

--- setup.py
+++ setup.py
@@ -6,38 +6,37 @@ readmepath = path.join(path.dirname(path.abspath(__file__)), 'README.rst')
 with open(readmepath, 'r') as readme:
     long_desc = readme.read()
 
-if __name__ == ' __main__':
-    setup(
-        name='reentry',
-        version='1.0',
-        author='Rico Häuselmann',
-        license='MIT License',
-        description='A plugin manager based on setuptools entry points mechanism',
-        long_description=long_desc,
-        packages=find_packages(),
-        include_package_data=True,
-        package_data={
-            '': ['js_data', 'README.rst']
-        },
-        entry_points={
-            'distutils.setup_keywords': [
-                'reentry_register = reentry.hooks:register_dist',
-                'reentry_scan = reentry.hooks:scan'
-            ],
-            'console_scripts': [
-                'reentry = reentry.cli:reentry'
-            ]
-        },
-        classifiers=[
-            'License :: OSI Approved :: MIT License',
-            'Programming Language :: Python',
-            'Development Status :: 5 - Production/Stable',
-            'Environment :: Plugins',
-            'Intended Audience :: Developers',
-            'Topic :: Software Development'
+setup(
+    name = 'reentry',
+    version = '1.0',
+    author = 'Rico Häuselmann',
+    license = 'MIT License',
+    description='A plugin manager based on setuptools entry points mechanism',
+    long_description=long_desc,
+    packages=find_packages(),
+    include_package_data=True,
+    package_data={
+        '': ['js_data', 'README.rst']
+    },
+    entry_points={
+        'distutils.setup_keywords': [
+            'reentry_register = reentry.hooks:register_dist',
+            'reentry_scan = reentry.hooks:scan'
         ],
-        install_requires={
-            'setuptools >= 18.5',
-            'click'
-        }
-    )
+        'console_scripts': [
+            'reentry = reentry.cli:reentry'
+        ]
+    },
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Plugins',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development'
+    ],
+    install_requires={
+        'setuptools >= 18.5',
+        'click'
+    }
+)
