bc74edba8f266743c964d5fdc3375448f83ba8d2
DropD <r.haeuselmann@gmx.ch>
2018-04-11 12:20:33 +0200

    yapf debug output for travis

--- run_ci_tests.sh
+++ run_ci_tests.sh
@@ -4,9 +4,7 @@ case "$TEST_TYPE" in
         tox
         ;;
     pre-commit)
-        pre-commit run --all-files
-        git diff
-        pip freeze | grep yapf
+        pre-commit run --all-files || git diff; pip freeze | grep yapf; exit 1
         ;;
     *)
         echo "Invalid value for TEST_TYPE env variable."
