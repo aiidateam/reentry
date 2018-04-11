6ec6e0d882850dd37d15cf31fb132c773b1324ee
DropD <r.haeuselmann@gmx.ch>
2018-04-11 12:28:34 +0200

    more debug output

--- .travis-tests/host-pkg/reentry_test_host/tests.py
+++ .travis-tests/host-pkg/reentry_test_host/tests.py
@@ -1,10 +1,15 @@
 """Test main for integration test, requires the test plugin to be installed first."""
+from py import path as py_path  # pylint: disable=no-name-in-module
+
 from reentry import manager
+from reentry.config import get_datafile
 
 if __name__ == '__main__':
     ENTRY_POINT_MAP = manager.get_entry_map(dist_names='reentry-test-plugin', groups='reentry_test', ep_names='test-plugin')
+    DATA_FILE = py_path.local(get_datafile())
 
-    assert ENTRY_POINT_MAP, 'The test plugin entry point was not found'
+    assert ENTRY_POINT_MAP, 'The test plugin entry point was not found\nMap:\n{}\n\nData File: {}\n\nContents:\n{}'.format(
+        manager.get_entry_map(), DATA_FILE.strpath, DATA_FILE.read())
     TEST_EP = ENTRY_POINT_MAP['reentry_test']['test-plugin']
 
     PLUGIN_CLASS = TEST_EP.load()
