2b4f4a7e05c2321bee4cfb1d2b7eb4c0c7cc3ed9
Leopold Talirz <leopold.talirz@gmail.com>
2019-04-08 23:28:40 +0200

    up yapf to 0.27

--- .travis-tests/noreg-pkg/setup.py
+++ .travis-tests/noreg-pkg/setup.py
@@ -2,7 +2,6 @@
 from setuptools import setup, find_packages
 
 if __name__ == '__main__':
-    setup(
-        name='reentry-test-noreg',
-        packages=find_packages(),
-        entry_points={'reentry_test': ['test-noreg = reentry_test_plugin.plugin:PluginClass']})
+    setup(name='reentry-test-noreg',
+          packages=find_packages(),
+          entry_points={'reentry_test': ['test-noreg = reentry_test_plugin.plugin:PluginClass']})
--- .travis-tests/plugin-pkg/setup.py
+++ .travis-tests/plugin-pkg/setup.py
@@ -2,8 +2,7 @@
 from setuptools import setup, find_packages
 
 if __name__ == '__main__':
-    setup(
-        name='reentry-test-plugin',
-        packages=find_packages(),
-        reentry_register=True,
-        entry_points={'reentry_test': ['test-plugin = reentry_test_plugin.plugin:PluginClass']})
+    setup(name='reentry-test-plugin',
+          packages=find_packages(),
+          reentry_register=True,
+          entry_points={'reentry_test': ['test-plugin = reentry_test_plugin.plugin:PluginClass']})
