4f8a81416d2f0a99eef134b9bf82bd4a0b5258ab
Leopold Talirz <leopold.talirz@gmail.com>
2019-04-10 13:10:40 +0200

    drop reentry_scan hook
    In pip 19, the isolation of the build environment means that
    pkg_resources in install hooks provides access only to the distributions
    in the build environment (setuptools, wheel, reentry + dependencies).
    
    The `reentry_scan` install hook was therefore no longer able to scan for
    entry points in the target environment and lost its purpose.
    
     * update changelog
     * fix #3: clarify meaning of "extras" in entry points in README

--- .travis-tests/host-pkg/setup.py
+++ .travis-tests/host-pkg/setup.py
@@ -5,7 +5,6 @@ if __name__ == '__main__':
     setup(name='reentry-test-host',
           packages=find_packages(),
           install_requires=['reentry'],
-          reentry_scan=['reentry_test'],
           reentry_register=True,
           entry_points={
               'console_scripts': ['reentry-test-hooks = reentry_test_host.tests:main'],
